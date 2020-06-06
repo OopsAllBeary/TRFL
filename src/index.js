2c436388f481b425085f26a57677ae87ac5e0296
OopsAllBeary <bearbdo@gmail.com>
2020-06-06 19:57:31 -0400

    game config and preloader

--- src/index.js
+++ src/index.js
@@ -1,21 +1,67 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+
+//images
+import blueman from "./assets/sprites/bluemansp/spritesheet.png";
+import bluemanWalk from "./assets/sprites/blueman-walk/spritesheet.png";
+import bluemanCheer from "./assets/sprites/blueman-cheer/spritesheet.png";
+import bluemanPunch from "./assets/sprites/blueman-punch/spritesheet.png";
+import punch from "./assets/sprites/fist/spritesheet.png";
+
+//sounds
+import punchSlap from "./assets/audio/slap.mp3";
+import punchStrong from "./assets/audio/spunch.mp3";
+import punchWoosh from "./assets/audio/woosh.mp3";
+
+
+
+
+
+let gameWidth = 800;
+let gameHeight = 600;
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: gameWidth,
+  height: gameHeight,
+  scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: true
+    }
+  },
+  parent: 'game-wrapper',
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    // update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  //Sprites
+  this.load.spritesheet('blueman', blueman, { frameWidth: 104, frameHeight: 92, endFrame: 16 });
+
+  this.load.spritesheet('blueman-walk', bluemanWalk, { frameWidth: 108, frameHeight: 94, endFrame: 192 });
+
+  this.load.spritesheet('blueman-cheer', bluemanCheer, { frameWidth: 122, frameHeight: 102, endFrame: 336 });
+
+  this.load.spritesheet('blueman-punch', bluemanPunch, { frameWidth: 124, frameHeight: 100, endFrame: 224 });
+
+  this.load.spritesheet('punch', punch, { frameWidth: 54, frameHeight: 104, endFrame: 16 });
+
+
+
+  //Audio
+  this.load.audio('punchSlap', punchSlap);
+  this.load.audio('punchStrong', punchStrong);
+  this.load.audio('punchWoosh', punchWoosh);
 }
 
 function create() {
